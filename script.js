73be55980966bf0c14db2668cb463edec655d80a
Austin Duran <amduran121@gmail.com>
2021-11-13 12:28:08 -0700

    push

--- script.js
+++ script.js
@@ -7,14 +7,21 @@ var currentTime = m.hour();
 
 //THis is the clock from moment.js. The function produces the clock in the area on the HTML with the id=currentDay. We are modifying the id=current day with the function clock.
 function clock() {
-    moment().format("MMMM Do YYYY, h:mm:ss a");
-    $("#currentDay").html(dateString);
-
+  moment().format("MMMM Do YYYY, h:mm:ss a");
+  $("#currentDay").html(dateString);
 }
 //This is setting the interval of the clock to update every 1000 miliseconds or 1 second
 setInterval(clock, 1000);
 
-//Here
-for (var hour = 9; hour <= 17; hour ++) {
-    scheduledHours.push(moment({hour}).format('h a'));
-} 
+//Here we have a loop to generate the text areas for scheduling this loop generates an area for a 1 hour period of time and continues to do until 5 pm is reaches as designated by the hour <=17.
+for (var hour = 9; hour <= 17; hour++) {
+  scheduledHours.push(moment({ hour }).format("h a"));
+  //Container is appended and is designated for such in the given html file.
+  $(".container").append(
+    <div class="row time block" data-time="${hour}">
+      <div class="col-sm col-md-2 hour">
+        <p>${moment({ hour }).format("h a")}</p>
+      </div>
+    </div>
+  );
+}
