423fca3569024a78cd2655a416e1b91f0cff9f1c
Austin Duran <amduran121@gmail.com>
2021-11-13 16:02:55 -0700

    push

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 var currentDay = document.getElementById("currentDay");
-
 currentDay.append(moment());
 
-<div class="container">
-  <div class="row align-items-start">
-    <div class="col">One of three columns</div>
-    <div class="col">One of three columns</div>
-    <div class="col">One of three columns</div>
-  </div>
-  <div class="row align-items-center">
-    <div class="col">One of three columns</div>
-    <div class="col">One of three columns</div>
-    <div class="col">One of three columns</div>
-  </div>
-  <div class="row align-items-end">
-    <div class="col">One of three columns</div>
-    <div class="col">One of three columns</div>
-    <div class="col">One of three columns</div>
-  </div>
-</div>;
+var timeOfDay = document.getElementsByClassName("timeOfDay");
+var timeColor = document.getElementsByClassName("timeColor");
+
+var determineTime = moment().format("HH");
+
+console.log(determineTime);
+
+var timeArray = {
+    document.getElementsByClassName
+}
+
+function changeColor (determineTime) {
+    if (timeOfDay < determineTime)
+}
