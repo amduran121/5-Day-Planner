9d16d18019f1b43809bcdccf8e49dac092f3b939
Austin Duran <amduran121@gmail.com>
2021-11-14 15:36:41 -0700

    final pushing

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ var newDay = moment().hour(0);
 var currentTime = m.hour();
 
 // adding clock to currentDay id
+function clock() {
   var dateString = moment().format("MMMM Do YYYY, h:mm:ss a");
   $("#currentDay").html(dateString);
+}
 
 setInterval(clock, 1000);
 
 //generating textareas for scheduling
 for (var hour = 9; hour < 18; hour++) {
   scheduledHours.push(moment({ hour }).format("h  a"));
-  $(".container").append(<div class='row time-block' data-time='${hour}'>
+  $(".container").append(`<div class='row time-block' data-time='${hour}'>
        <!--hour column-->
            <div class='col-sm col-md-2 hour'>
              <p>${moment({ hour }).format("h  a")}</p>
@@ -30,69 +32,90 @@ for (var hour = 9; hour < 18; hour++) {
                 </div>
               </div>
             </div>
-          </div>);
+          </div>`);
 }
+
 //Checking time to determine present, past, or future
-$.each($('.time-block'), function(index, value) {
-  let dateHour = $(value).attr('data-time');
+$.each($(".time-block"), function (index, value) {
+  let dateHour = $(value).attr("data-time");
   if (Number(dateHour) === m.hour()) {
-    $(this).find('textarea').addClass('present');
+    $(this).find("textarea").addClass("present");
   } else if (Number(dateHour) < m.hour()) {
-    $(this).find('textarea').addClass('past').attr('disabled', 'disabled');
-    $(this).find('.save-button').addClass('disabled').attr('disabled', true);
+    $(this).find("textarea").addClass("past").attr("disabled", "disabled");
+    $(this).find(".save-button").addClass("disabled").attr("disabled", true);
   } else {
-    $(this).find('textarea').addClass('future');
+    $(this).find("textarea").addClass("future");
   }
 });
 
 console.log(currentTime);
 
-if (currentTime >=0 && currentTime < 9){
+if (currentTime >= 0 && currentTime < 9) {
   localStorage.clear();
-  
+}
 
-  //If there are items in local storage, get those items...
-  if (localStorage.getItem('availableHours')) {
-      //...those items being set to available hours...
-      // parse them into objects, from local storage 
-  availableHours = JSON.parse(localStorage.getItem('availableHours'));
+//Check for local storage to set value to the object and clearing if currentTime is between 12am and 9am
+if (localStorage.getItem("availableHours")) {
+  availableHours = JSON.parse(localStorage.getItem("availableHours"));
 } else {
   availableHours = {
-    '9': {
-      time: '9',
-      value: ''
+    9: {
+      time: "9",
+      value: "",
     },
-    '10': {
-      time: '10',
-      value: ''
+    10: {
+      time: "10",
+      value: "",
     },
-    '11': {
-      time: '11',
-      value: ''
+    11: {
+      time: "11",
+      value: "",
     },
-    '12': {
-      time: '12',
-      value: ''
+    12: {
+      time: "12",
+      value: "",
     },
-    '13': {
-      time: '13',
-      value: ''
+    13: {
+      time: "13",
+      value: "",
     },
-    '14': {
-      time: '14',
-      value: ''
+    14: {
+      time: "14",
+      value: "",
     },
-    '15': {
-      time: '15',
-      value: ''
+    15: {
+      time: "15",
+      value: "",
     },
-    '16': {
-      time: '16',
-      value: ''
+    16: {
+      time: "16",
+      value: "",
     },
-    '17': {
-      time: '17',
-      value: ''
-    }
+    17: {
+      time: "17",
+      value: "",
+    },
   };
 }
+
+//set value of availableHours to equal the user input for each row
+$(".time-block").each(function () {
+  $(this)
+    .find(".text-area")
+    .val(availableHours[$(this).attr("data-time")].value);
+});
+
+//save value to local storage on click
+$(".save-button").on("click", function (event) {
+  event.preventDefault();
+
+  //set availableHours time attribute
+  var timeValue = $(this).closest(".time-block").attr("data-time");
+
+  //set availableHours value attribute
+  var textValue = $(this).closest(".time-block").find(".text-area").val();
+  availableHours[timeValue].value = textValue;
+
+  //save user input in each object to local storage
+  localStorage.setItem("availableHours", JSON.stringify(availableHours));
+});
